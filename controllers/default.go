8c29965f38b4e0550f95051eba188cb1f42b8d4c
Yugovtr <vtrhg69@hotmail.com>
2018-05-07 17:03:58 -0300

    Add layout

--- controllers/default.go
+++ controllers/default.go
@@ -11,5 +11,11 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Github"] = "Yugovtr"
 	c.Data["Email"] = "vitormirandawork@gmail.com"
+	c.Layout = "Layout/Default/layout.html"
 	c.TplName = "index.html"
+
+	c.LayoutSections = make(map[string]string)
+    c.LayoutSections["HtmlHead"] = "Layout/Default/html_head.html"
+    c.LayoutSections["Scripts"] = "Layout/Default/scripts.html"
+    c.LayoutSections["Sidebar"] = ""
 }
